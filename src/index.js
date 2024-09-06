023afe9632da0162cfdca235727783f8c7633066
monirulislamkhan <mr.monirulkhan@gmail.com>
2024-09-06 12:42:11 +0530

    webpack-with-tailwind setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
+/* import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 
-console.log('hi it is production mode');
+import 'swiper/css';
+// import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+}); */
+
+// console.log('hi it is production mode');
